2f2131ab29cab4435f0d2351697d3faacbd88e71
Nikola Hristov <nikola@nikolahristov.tech>
2022-10-17 18:26:56 +0300

    Cleanup

--- dist/command/squash.d.ts
+++
@@ -1,2 +0,0 @@
-declare const squash: () => Promise<void>;
-export default squash;
--- dist/lib/git-directories.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * It takes a set of globs, and returns a map of git directories to the globs that are in that
- * directory
- * @param globs - Set<string>
- * @returns A map of git directories to a set of globs.
- */
-declare const gitDirectories: (globs: Set<string>) => Promise<Map<any, any>>;
-export default gitDirectories;
